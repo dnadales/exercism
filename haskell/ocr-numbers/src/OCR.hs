2c29d8f0d3fdeb4a9efa58531d4ed365fd491f20
Damian Nadales <damian.nadales@gmail.com>
2017-03-08 16:37:04 +0100

    Implemented up to step four.

--- haskell/ocr-numbers/src/OCR.hs
+++ haskell/ocr-numbers/src/OCR.hs
@@ -3,17 +3,37 @@ module OCR (convert) where
 import Data.Maybe
 
 convert :: String -> String
-convert = fromMaybe "?" . (ocrDigit . lines)
---  = error "You need to implement this function."
+convert xss = (parseLine (lines xss)) >>= ocrDigit
+
+
+
+newtype DigitGrid = DigitGrid [String]
+
+-- | Try to divide the input into a sequence of 4x3 (rows x cols) matrices.
+parseLine :: [String] -> [DigitGrid]
+parseLine [w0:w1:w2:ws, x0:x1:x2:xs, y0:y1:y2:ys, z0:z1:z2:zs] =
+  DigitGrid [ [w0, w1, w2]
+            , [x0, x1, x2]
+            , [y0, y1, y2]
+            , [z0, z1, z2] ] : parseLine [ws, xs, ys, zs]
+parseLine _ = []
 
 -- | Try to recognize a single digit.
-ocrDigit :: [String] -> Maybe String
-ocrDigit xs 
-  | xs == repZero = Just "0"
-  | xs == repOne  = Just "1"
-  | otherwise     = Nothing
+ocrDigit :: DigitGrid -> String
+ocrDigit (DigitGrid xs)
+  | xs == repZero  = "0"
+  | xs == repOne   = "1"
+  | xs == repTwo   = "2"
+  | xs == repThree = "3"
+  | xs == repFour  = "4"
+  | xs == repFive  = "5"
+  | xs == repSix   = "6"
+  | xs == repSeven = "7"
+  | xs == repEight = "8"
+  | xs == repNine  = "9"    
+  | otherwise      = "?"
 
--- Representation of the numbers:
+-- * presentation of the numbers:
 repZero = [ " _ "
           , "| |"
           , "|_|"
